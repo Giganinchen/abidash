31d260289ff2e2355e8904fe22cfcd8b21c55a96
Giganinchen <99274869+Giganinchen@users.noreply.github.com>
2024-08-27 21:48:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ FOLDER_TEMPLATE = '''<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PDF Übersicht - {folder_name}</title>
+    <title>Übersicht - {folder_name}</title>
     <style>
         body {{
             font-family: Arial, sans-serif;
